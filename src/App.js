35b760b8f053a9c9da601cd4d1b829ee53f22116
EMardanov <emardanov@o365.comhard.de>
2024-10-29 15:26:32 +0100

    Like-Button mit Zähler erstellt

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
+import React, {Component} from 'react';
+
 import logo from './logo.svg';
 import './App.css';
+
+
+class Message extends Component{
+  render() {
+    return <p>{this.props.message}</p>
+  }
+}
+
+function MessageFunky(props){
+  return <p>{props.message}</p>
+}
+
+
+function LikeButton (){
+  const[likes, setLikes] = React.useState(0); //Initialwert 0 für 'likes'.
+
+  function handleClick(){
+      setLikes(likes + 1);
+  }
+
+  return <button onClick={handleClick}>Like--({likes})--</button>
+}
 
 function App() {
   return (
     <div className="App">
+      <LikeButton  />
+      <Message message="classic"/>
+      <MessageFunky message="funky" />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          I do it.
         </p>
         <a
           className="App-link"
